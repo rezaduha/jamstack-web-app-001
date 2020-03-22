fcf5270bea2693aba9d2b439c95cd49eb9980b10
rezaduha <rezafauzizisiwaduha@gmail.com>
2020-03-22 12:01:35 +0700

    implement dotenv in news.js

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
+require('dotenv').config();
 
 module.exports = async function () {
     try {
-      const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=c96898cdc1c8417c8b15dfac8004c4a3&pageSize=5`);
+      const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
       return response.data;
     } catch (error) {
       console.error(error);
