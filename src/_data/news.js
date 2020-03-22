0ca9113c7fc3333087b61f12001d9e1d4b7c2c25
rezaduha <rezafauzizisiwaduha@gmail.com>
2020-03-22 11:47:21 +0700

    implement eleventy into news.js

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-async function getUser() {
+module.exports = async function () {
     try {
       const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=us&apiKey=c96898cdc1c8417c8b15dfac8004c4a3&pageSize=5`);
       return response.data;
